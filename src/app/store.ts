5f808187cfd4c897c8f8e78210f1871bb91e33bc
Anna-Daryna Sobko <dar.sobko@gmail.com>
2025-09-03 18:16:23 +0300

    updated2

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,9 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit';
 import { todosSlice } from '../features/todos';
-import { isLoadingSlice } from '../features/isLoading';
 import { filterSlice } from '../features/filter';
 import { currentTodoSlice } from '../features/currentTodo';
 
-const rootReducer = combineSlices(
-  todosSlice,
-  isLoadingSlice,
-  filterSlice,
-  currentTodoSlice,
-);
+const rootReducer = combineSlices(todosSlice, filterSlice, currentTodoSlice);
 
 export const store = configureStore({
   reducer: rootReducer,
