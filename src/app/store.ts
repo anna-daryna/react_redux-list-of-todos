08692eb44b343a43bae3938c26ddf316b354a635
Anna-Daryna Sobko <dar.sobko@gmail.com>
2025-09-03 17:06:45 +0300

    add task solution

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,15 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit';
+import { todosSlice } from '../features/todos';
+import { isLoadingSlice } from '../features/isLoading';
+import { filterSlice } from '../features/filter';
+import { currentTodoSlice } from '../features/currentTodo';
 
-const rootReducer = combineSlices();
+const rootReducer = combineSlices(
+  todosSlice,
+  isLoadingSlice,
+  filterSlice,
+  currentTodoSlice,
+);
 
 export const store = configureStore({
   reducer: rootReducer,
